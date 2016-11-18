5c1097e6521509ffb075dfa6ed203b30c2650ee4
MargoDrozd <drozdmargo@i.ua>
2016-11-18 14:44:52 +0200

    mean-stack app

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+	'database': 'mongodb://root:123abc@ds015929.mlab.com:15929/userstory',
+	'port': process.env.PORT || 3000,
+	'secretKey': 'YourSecretKey',
+
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require ('express');
+var bodyParser = require ('body-parser');
+var morgan = require ('morgan');
+var config = require ('./config.js');
+var mongoose = require ('mongoose');
+var Promise = require("bluebird");
+
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+mongoose.connect(config.database, function(err) {
+	if(err) {
+		console.log(err);
+	} else {
+		console.log('Connected to the database');
+	}
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+var api = require('./app/routes/api')(api, express, io);
+app.use('/api', api);
+
+app.get('*', function(req, res) {
+	res.sendFile(__dirname + '/public/app/views/index.html');
+});
+
+http.listen( config.port, function(err) {
+	if(err) {
+		console.log(err);
+	}else {
+		console.log('listening on port 3000')
+	}
+});
